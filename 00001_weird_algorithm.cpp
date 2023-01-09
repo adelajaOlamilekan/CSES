2658ccac958b94f0110230821ad3d319e3c4b568
Adelaja Qowiyyu <adexolamilekan46@gmail.com>
2023-01-09 12:37:02 +0100

    Added a new approach of bit manipulation

--- 00001_weird_algorithm.cpp
+++ 00001_weird_algorithm.cpp
@@ -4,10 +4,12 @@ Consider an algorithm that takes as input a positive integer n. If n is even, th
 
 Your task is to simulate the execution of the algorithm for a given value of n.
 
-Approach:   Check if n is odd or even, 
-            perform the appropriate operation, 
-            update n,
-            repeat until n is 1
+Approach1(Arithmetic Computation):      Check if n is odd or even, 
+                                        perform the appropriate operation, 
+                                        update n,
+                                        repeat until n is 1
+
+Approach2(Bit Manipulation):            Approach1 but the mathematical operation are done with bit manipulation
 
 Time complexity:    O(n)
 Space complexity:   O(1)
@@ -15,18 +17,37 @@ Space complexity:   O(1)
 
 #include<iostream>
 
-int main(){
-    long long n = 0;
-    std::cin >> n;
+//Approach1
+// int main(){
+//     long long n = 0;
+//     std::cin >> n;
 
-    while(n !=  1){
-        std::cout << n << " ";
-        //updating n based on its parity
-        n = n%2==0 ? n/2 : n*3+1;
-    }
-    std::cout << n << " ";
+//     while(n !=  1){
+//         std::cout << n << " ";
+//         //updating n based on its parity
+//         n = n%2==0 ? n/2 : n*3+1;
+//     }
+//     std::cout << n << " ";
 
-    return 0;
+//     return 0;
+// }
+
+//Approach 2
+int main()
+{
+	long long n;
+	std::cin >> n;
+	cout << n << " ";
+
+	while (n > 1)
+	{
+		if (n & 1)      //checks for odd parity
+			n = (3 * n) + 1;
+		else
+			n >>= 1;    //Divide n by 2
+		cout << n << " ";
+	}
+	return 0;
 }
 
 
