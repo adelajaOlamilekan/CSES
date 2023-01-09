cc756b7477c95f0cf8c365711bcd7e4b29e67056
Adelaja Qowiyyu <adexolamilekan46@gmail.com>
2023-01-09 06:52:53 +0100

    I will become an ICPC Champ

---
+++ 00002_missing_number.cpp
@@ -0,0 +1,35 @@
+/*
+You are given all numbers between 1,2,…,n except one. 
+Your task is to find the missing number.
+
+Approach1:  Sort the number
+            While traversing the list:
+            the missing number is found when next number is not 1 from previous number
+
+*/
+
+#include<iostream>
+#include<vector>
+
+int main(){
+    int n = 0, currentNo = 0;
+    std::cin>>n;
+
+    //Setting the default value(zero) for each number position
+    std::vector<int> nums(n, 0);
+
+    //Setting the values of the vector
+    while(std::cin>>currentNo)
+        nums[currentNo-1] = currentNo;
+
+    //Checking for the missing value
+    int IndexOfpotentialMissingValue = 0;
+    while(nums[IndexOfpotentialMissingValue] != 0 && IndexOfpotentialMissingValue < n)
+        ++IndexOfpotentialMissingValue;
+    
+    //Printing the missing value;
+    int actualMissingValue = IndexOfpotentialMissingValue+1;
+    std::cout << actualMissingValue;
+
+    return 0;
+}
